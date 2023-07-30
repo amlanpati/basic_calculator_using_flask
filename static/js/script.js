8f3e93cb49396e7a4ab1c2062cb8ffbf63d54127
amlanpati <amlanpati98@gmail.com>
2023-07-30 17:52:33 +0530

    Uploaded Calculator Project

---
+++ static/js/script.js
@@ -0,0 +1,16 @@
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function calculate() {
+    try {
+        const result = eval(document.getElementById('display').value);
+        document.getElementById('display').value = result;
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
